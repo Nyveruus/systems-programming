8cc83c6ea759de0885d51f87a1bdf35a3a655c3f
Cementarii <nikomir32@gmail.com>
2026-02-26 19:27:21 +0100

    run loop, pcap header, packet header, recvfrom

--- projects/networking/packet-sniffer/main.c
+++ projects/networking/packet-sniffer/main.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <unistd>
+#include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
 
@@ -11,12 +11,34 @@
 #include <sys/ioctl.h>
 
 #include <signal.h>
+#include <time.h>
+#include <sys/time.h>
 
 #define MAX_IF 32
+
+struct __attribute__((packed)) pcap_header {
+    uint32_t magic_number;
+    uint16_t version_major;
+    uint16_t version_minor;
+    int32_t reserved1;
+    uint32_t reserved2;
+    uint32_t snaplen;
+    uint32_t link_type;
+};
+
+struct __attribute__((packed)) packet_header {
+    uint32_t ts_sec;
+    uint32_t ts_usec;
+    uint32_t incl_len;
+    uint32_t orig_len;
+};
 
 static char *promisc_interfaces[MAX_IF];
 static int promisc_count = 0;
 static int keep_running = 1;
+
+void set_promisc(char *arg, int socket_fd);
+void unset_promisc(char *arg, int socket_fd);
 
 void signal_handler(int sig) {
     (void)sig;
@@ -47,6 +69,7 @@ int main(int argc, char *argv[]) {
         ifnidxs = if_nameindex();
         if (ifnidxs == NULL) {
             perror("if_nameindex");
+            close(socket_fd);
             return 1;
         }
         for (intf = ifnidxs; intf->if_index != 0 || intf->if_name != NULL; intf++) {
@@ -55,16 +78,86 @@ int main(int argc, char *argv[]) {
         }
         if_freenameindex(ifnidxs);
     }
-
     //open .pcap file wb
+    FILE *file = fopen("captures/file.pcap", "wb");
+    if (!file) {
+        perror("fopen");
+        goto cleanup;
+    }
     //write header to .pcap file
+    struct pcap_header phdr = {
+        .magic_number = 0xA1B2C3D4,
+        .version_major = 2,
+        .version_minor = 4,
+        .reserved1 = 0,
+        .reserved2 = 0,
+        .snaplen = 65535,
+        .link_type = 1
+    };
+    fwrite(&phdr, sizeof(phdr), 1, file);
+
     //while running: recvfrom, function: check if from right interface (filter for default or user arg interfaces) write to .pcap
-    //cleanup: remove promisc mode from interfaces, close socket, free any
+    unsigned char buffer[65536];
+    time_t start_time = time(NULL);
+
+    while (keep_running) {
+        //while running: if time elapsed over X hours, overwrite file (close and open) and rebuild headers
+        if (time(NULL) - start_time >= 7200) {
+            fclose(file);
+            file = fopen("captures/file.pcap", "wb");
+            if (!file) {
+                perror("fopen");
+                goto cleanup;
+            }
+            fwrite(&phdr, sizeof(phdr), 1, file);
+            start_time = time(NULL);
+        }
+
+        struct sockaddr_ll src_addr;
+        socklen_t addr_len = sizeof(src_addr);
+        ssize_t len = recvfrom(socket_fd, buffer, sizeof(buffer), 0, (struct sockaddr*)&src_addr, &addr_len);
+        if (len < 0) {
+            if (keep_running)
+                perror("recvfrom");
+            break;
+        } else if (len == 0 || len > 65535) continue;
+
+        char ifname[IFNAMSIZ];
+        if_indextoname(src_addr.sll_ifindex, ifname);
+        if (argc > 1) {
+            int match = 0;
+            for (int i = 1; i < argc; i++)
+                if (strcmp(ifname, argv[i]) == 0) {
+                    match = 1;
+                    break;
+                }
+            if (!match) continue;
+        } else {
+            if (strcmp(ifname, "lo") == 0) continue;
+        }
+
+        struct timeval tv;
+        gettimeofday(&tv, NULL);
+
+        struct packet_header pkt_hdr = {
+            .ts_sec = (uint32_t)tv.tv_sec,
+            .ts_usec = (uint32_t)tv.tv_usec,
+            .incl_len = (uint32_t)len,
+            .orig_len = (uint32_t)len
+        };
+
+        fwrite(&pkt_hdr, sizeof(pkt_hdr), 1, file);
+        fwrite(buffer, len, 1, file);
+        fflush(file);
+    }
+    //cleanup: remove promisc mode from interfaces, close socket, free
+cleanup:
     for (int i = 0; i < promisc_count; i++) {
         unset_promisc(promisc_interfaces[i], socket_fd);
         free(promisc_interfaces[i]);
     }
     close(socket_fd);
+    if (file) fclose(file);
     return 0;
 }
 
